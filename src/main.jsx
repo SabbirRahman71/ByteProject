e21e58127b141119f69ea58e7a63964298f189b4
SabbirRahman71 <mdsabbirrahmanbd71@gmail.com>
2024-10-01 22:54:47 +0600

    Initial setup complete

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+// import Home from "./Pages/Home";
+import Bookmarks from "./Pages/Bookmarks";
+import Blogs from "./Pages/Blogs";
+import MainLayout from "./Layouts/MainLayout";
+import Home from "./Pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "/bookmarks",
+        element: <Bookmarks></Bookmarks>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
