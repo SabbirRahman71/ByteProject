2900a01861fc56635809e9a722b2a3c290400d5f
SabbirRahman71 <mdsabbirrahmanbd71@gmail.com>
2024-10-08 02:07:13 +0600

    content,author and spinner added

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import Bookmarks from "./Pages/Bookmarks";
-import Blogs from "./Pages/Blogs";
-import MainLayout from "./Layouts/MainLayout";
-import Home from "./Pages/Home";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home></Home>,
-      },
-      {
-        path: "/blogs",
-        element: <Blogs></Blogs>,
-        loader: () => fetch("https://dev.to/api/articles?per_page=20&top=7"),
-      },
-      {
-        path: "/bookmarks",
-        element: <Bookmarks></Bookmarks>,
-      },
-    ],
-  },
-]);
+import { router } from "./Routes/Routes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
